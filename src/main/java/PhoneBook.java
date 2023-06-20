6516d02008ae356ef6c31b980e89f4091b59511e
Sobolev Sergey <shutle@gmail.com>
2023-06-20 20:39:26 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    public Map<String, String> byName = new TreeMap<>();
+    public Map<String, String> byNumber = new HashMap<>();
     int add(String name, String number) {
-        return 0;
+        byNumber.put(number, name);
+        byName.put(name, number);
+        return byName.size();
     }
 }
